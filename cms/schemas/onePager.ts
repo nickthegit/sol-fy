6cdb21997ee11fdcb3ddd65d84bf0b9ffc26f5a0
Nick <nickjohnmail@gmail.com>
2023-04-30 13:49:39 +0100

    install Sanity

---
+++ cms/schemas/onePager.ts
@@ -0,0 +1,36 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'onePager',
+  title: 'One Pager',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'heroImage',
+      title: 'Hero Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'bio',
+      title: 'Bio',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+        },
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+})
